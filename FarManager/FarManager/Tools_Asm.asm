4fa1a866710391f96a3ad747bab1126f729add53
Sam3peoplelife <83673324+Sam3peoplelife@users.noreply.github.com>
2025-06-26 18:05:24 +0200

    Init

---
+++ FarManager/FarManager/Tools_Asm.asm
@@ -0,0 +1,12 @@
+.code
+
+;-----------------------------------------------------------------------------------------------
+Draw_Line proc
+;extern "C" void Draw_Line(CHAR_INFO *screen_buffer, SPos spos, int len, CHAR_INFO symbol);
+	
+
+	ret
+Draw_Line endp
+;-----------------------------------------------------------------------------------------------
+
+end
